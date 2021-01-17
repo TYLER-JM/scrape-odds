3782b534b01d9d98a46a9fc0decc535a80683c67
Tyler Martin <tyler.martin@electric-playground.com>
2021-01-17 12:50:19 -0400

    adds converter function to convert to decimal odds

--- src/scrape.js
+++ src/scrape.js
@@ -3,13 +3,28 @@ const axios = require('axios');
 
 const { JSDOM } = jsdom;
 
+const convertOdds = (americanOdds) => {
+  if (!!Number(americanOdds)) {
+    if (Number(americanOdds) > 0) {
+      let converted = (Number(americanOdds) / 100) + 1;
+      return converted.toFixed(2);
+    } else {
+      let converted = (100 / Math.abs(Number(americanOdds))) + 1;
+      return converted.toFixed(2);
+    }
+  }
+};
+
 (async () => {
   let response = await axios.get('https://www.oddsshark.com/nba/odds');
   let dom = await new JSDOM(response.data);
   let oddsRows = dom.window.document.getElementsByClassName('op-item-row-wrapper not-futures');
 
+  let results = [];
   for (row of oddsRows[0].childNodes) {
-    console.log('FIRST CHILD', row.firstChild.lastChild.textContent);
-    console.log('SECOND CHILD', row.lastChild.lastChild.textContent);
+    let visitorOdds = row.firstChild.lastChild.textContent;
+
+      console.log('Visitor after Conversion: ', convertOdds(visitorOdds));
+    // console.log('SECOND CHILD', row.lastChild.lastChild.textContent);
   }
 })()
